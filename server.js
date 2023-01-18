0f0ea583cf481e3a18440e6fd4ade564a237d6d1
Brennan Malone <malone.brennan98@gmail.com>
2023-01-18 05:45:48 -0500

    implemented weather request and response

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ console.log('my server');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+let data = require('./data/weather.json')
 
 // **** USE EXPRESS ****
 // **** app === server ****
@@ -29,6 +30,41 @@ const PORT = process.env.PORT || 3002;
 app.get('/', (rquest, response) => {
   response.status(200).send('Welcome to my server');
 });
+
+app.get('/hello', (request, response) => {
+  console.log(request.query);
+
+  let firstName = request.query.firstName;
+  let lastName = request.query.lastName;
+  response.status(200).send(`Hello ${firstName} ${lastName}! Welcome to my server`);
+});
+
+app.get('/weather', (request, response, next) => {
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let searchQuery = request.searchQuery;
+
+    let dataToGroom = data.find(city => city.searchQuery === searchQuery);
+    let newArray = [];
+    for (let i = 0; i < dataToGroom.data.length; i++) {
+      newArray.push(new Forecast(dataToGroom.data[i]));
+    }
+    console.log(newArray);
+    response.status(200).send(newArray);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// **** CLASS TO GROOM BULKY DATA ****
+
+class Forecast {
+  constructor(weatherObj){
+    this.date = weatherObj.datetime;
+    this.description = weatherObj.weather.description;
+  }
+}
 
 // **** CATCH ALL ENDPOINT
 
